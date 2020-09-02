08bf0585595f37d738b232275285e9590572b9cc
Eric Sun <sunyonglincanada@gmail.com>
2020-09-01 23:03:44 -0400

    Update app descriptions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-// function App() {
-//     return <div className="App">Hello React - TypeScript!</div>;
-// }
-
 type AppProps = { message: string }; /* could also use interface */
 const App = ({ message }: AppProps) => <div>{message}</div>;
 
